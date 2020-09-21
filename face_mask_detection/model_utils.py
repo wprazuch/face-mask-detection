51c54202d2c8b49936a6dcae9129b7821db10f2a
WojPrauch <wojciechprazuch3@gmail.com>
2020-09-21 10:27:34 +0200

    Migrated code from notebooks to python modules

---
+++ face_mask_detection/model_utils.py
@@ -0,0 +1,138 @@
+
+import numpy as np
+import tensorflow as tf
+
+import os
+
+from tensorflow.keras.models import load_model
+import cv2
+
+from face_mask_detection import utils
+
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.preprocessing.image import img_to_array
+
+
+def check_face_masks(image, face_detector, facemask_classifier):
+    """Searches for faces in the image and returns class of the face,
+    whether it does have a mask on it or not
+
+    Parameters
+    ----------
+    image : [type]
+        [description]
+    face_detector : [type]
+        [description]
+    facemask_classifier : [type]
+        [description]
+
+    Returns
+    -------
+    list
+        list of dicts with detection-classification metadata of every face in the image
+    """
+
+    detections = detect_faces(image, face_detector)
+
+    image_detection_classification_results = []
+
+    for i in range(0, detections.shape[2]):
+
+        confidence = detections[0, 0, i, 2]
+
+        if confidence > 0.5:
+            detection = detections[0, 0, i, 3:7]
+            face, detection_info = extract_face(image, detection)
+
+            (mask, withoutMask) = facemask_classifier.predict(face)[0]
+
+            label = "Mask" if mask > withoutMask else "No Mask"
+
+            detection_info['class'] = label
+
+            detection_info['probability'] = max(mask, withoutMask) * 100
+
+            image_detection_classification_results.append(detection_info)
+
+    return image_detection_classification_results
+
+
+def detect_faces(image, face_detector):
+    """Detects faces in the image
+
+    Parameters
+    ----------
+    image : [type]
+        [description]
+    face_detector : [type]
+        [description]
+
+    Returns
+    -------
+    [type]
+        [description]
+    """
+    blob = cv2.dnn.blobFromImage(image, 1.0, (300, 300), (104.0, 177.0, 123.0))
+    face_detector.setInput(blob)
+    detections = face_detector.forward()
+    return detections
+
+
+def extract_face(image, detection):
+    """Extracts face from the image
+
+    Parameters
+    ----------
+    image : [type]
+        [description]
+    detection : [type]
+        [description]
+
+    Returns
+    -------
+    np.ndarray, dict
+        Returns cropped image with the face, together with bounding box cords of it in the image
+    """
+    (h, w) = image.shape[:2]
+    box = detection * np.array([w, h, w, h])
+    (startX, startY, endX, endY) = box.astype("int")
+
+    (start_x, start_y) = (max(0, startX), max(0, startY))
+    (end_x, end_y) = (min(w - 1, endX), min(h - 1, endY))
+
+    bounding_box_cords = {
+        'start_x': start_x,
+        'start_y': start_y,
+        'end_x': end_x,
+        'end_y': end_y
+    }
+
+    face = image[startY:endY, startX:endX]
+    face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+    face = cv2.resize(face, (224, 224))
+    face = img_to_array(face)
+    face = preprocess_input(face)
+    face = np.expand_dims(face, axis=0)
+
+    return face, bounding_box_cords
+
+
+def load_caffe_model(caffe_model_path):
+    """Loads model in caffe format from the path
+
+    Parameters
+    ----------
+    caffe_model_path : [type]
+        [description]
+
+    Returns
+    -------
+    [type]
+        [description]
+    """
+    prototxt_file = [file for file in os.listdir(caffe_model_path) if file.endswith('.prototxt')][0]
+    caffe_model_file = [file for file in os.listdir(
+        caffe_model_path) if file.endswith('.caffemodel')][0]
+    model = cv2.dnn.readNet(os.path.join(caffe_model_path, prototxt_file),
+                            os.path.join(caffe_model_path, caffe_model_file))
+    return model
--- face_mask_detection/utils.py
+++ face_mask_detection/utils.py
@@ -4,9 +4,26 @@ from tensorflow.image import resize
 from sklearn.preprocessing import OneHotEncoder
 import numpy as np
 import os
+import cv2
 
 
 def prepare_dataset(dataset_path, input_shape, class_mapping):
+    """Prepares dataset for training facemask classification network
+
+    Parameters
+    ----------
+    dataset_path : [type]
+        [description]
+    input_shape : [type]
+        [description]
+    class_mapping : [type]
+        [description]
+
+    Returns
+    -------
+    [type]
+        [description]
+    """
 
     dir_classes = os.listdir(dataset_path)
     data = []
@@ -27,14 +44,75 @@ def prepare_dataset(dataset_path, input_shape, class_mapping):
 
 
 def preprocess_image(image, input_shape):
+    """Preprocesses image for face-mask classification
+
+    Parameters
+    ----------
+    image : [type]
+        [description]
+    input_shape : [type]
+        [description]
+
+    Returns
+    -------
+    [type]
+        [description]
+    """
     image = preprocess_input(image)
     image = resize(image, input_shape)
     return image
 
 
 def to_one_hot(labels):
+    """Converts numpy array with class labels to one-hot-encoding
+
+    Parameters
+    ----------
+    labels : np.array (n_observations,)
+        class labels
+
+    Returns
+    -------
+    np.ndarray (n_observations, n_classes)
+        [description]
+    """
     onehot = OneHotEncoder()
     labels = np.array(labels).reshape(-1, 1)
     labels_one_hot = onehot.fit_transform(labels).toarray()
 
     return labels_one_hot
+
+
+def visualize_detection_classification_results(image, detection_classification_results):
+    """Returns image with bounding boxes around faces annotated with class labels
+
+    Parameters
+    ----------
+    image : [type]
+        [description]
+    detection_classification_results : [type]
+        [description]
+
+    Returns
+    -------
+    [type]
+        [description]
+    """
+
+    for result in detection_classification_results:
+
+        label = result['class']
+
+        color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
+
+        # include the probability in the label
+        label = "{}: {:.2f}%".format(label, result['probability'])
+
+        # display the label and bounding box rectangle on the output
+        # frame
+        cv2.putText(image, label, (result['start_x'], result['start_y'] - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
+        cv2.rectangle(image, (result['start_x'], result['start_y']),
+                      (result['end_x'], result['end_y']), color, 2)
+
+    return image
